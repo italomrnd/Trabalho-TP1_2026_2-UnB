eb8ac945d407610287d7b63f939ed01bc26d0591
Italo Miranda <italosobrenomemiranda@gmail.com>
2026-08-25 17:59:10 -0300

    Conclusao das classes de dominio no dominios.hpp

--- dominios.hpp
+++ dominios.hpp
@@ -1,17 +1,140 @@
-#include <iostream>
+#ifndef DOMINIOS_HPP
+#define DOMINIOS_HPP
+
+
 #include <string>
 using namespace std;
 
-class NOME{
+
+class Email{
     private:
-        string Nome;
-        bool validar(string);
+        string valor;
+        void validar(string);
 
     public:
-        string getNome();
-        bool setValor(string);
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
 };
 
-inline string NOME :: getNome(){
-    return Nome;
-}
+class Estado{
+    private:
+        string valor;
+        void validar(string);
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+class Identificador{
+    private:
+        string valor;
+        void validar(string);
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+class Limite{
+    private:
+        int valor;
+        void validar(int);
+    public:
+        int getValor() const{
+            return valor;
+        }
+        void setValor(int);
+};
+
+class Nome{
+    private:
+        string valor;
+        void validar(string); 
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+class Papel{
+    private:
+        string valor;
+        void validar(string);
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+class Prioridade{
+    private:
+        string valor;
+        void validar(string);
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+class Senha{
+    private:
+        string valor;
+        void validar(string);
+    
+    public:
+        string getValor() const{
+                return valor;
+        }
+        void setValor(string);
+};
+
+class Tamanho{
+    private:
+        string valor;
+        void validar(string);
+    
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);      
+};
+
+class Texto{
+    private:
+        string valor;
+        void validar(string);
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+class Timestamp{
+    private:
+        string valor;
+        void validar(string);
+
+    public:
+        string getValor() const{
+            return valor;
+        }
+        void setValor(string);
+};
+
+#endif // DOMINIOS_HPP
