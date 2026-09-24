ff61ef014b2e938d5c8ccf9fe3af2fc0a9b61ecd
Italo Miranda <italosobrenomemiranda@gmail.com>
2026-09-24 12:13:09 -0300

    feat: (re)criacao dos arquivos, prontos para trabalhar

--- Headers/testes.hpp
+++ Headers/testes.hpp
@@ -1,1 +1,16 @@
-// Arquivo header dos testes
+#ifndef TESTES_HPP
+#define TESTES_HPP
+
+#include <stdexcept>
+#include <string>
+
+// Declaracao dos testes de dominios
+
+
+
+
+
+
+// Declaracao dos testes de entidades
+
+#endif
--- Sources/testes.cpp
+++ Sources/testes.cpp
@@ -1,1 +1,14 @@
-// Arquivo de testes
+#include "testes.hpp"
+#include <stdexcept>
+#include <string>
+
+// Implementacao dos metodos de testes de dominios
+
+
+
+
+
+
+
+
+// Implementacao dos metodos de testes de entidades
